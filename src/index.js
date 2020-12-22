1b66e17c9d30c5408b7a7a59d1dd763b194b87f3
Ben Liu <benliu.rbhs@gmail.com>
2020-12-21 23:00:12 -0500

    test

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const AppContainer = () => {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default AppContainer;
